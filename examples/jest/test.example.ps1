117e84cc7f49488c5541490647ca92a16b71e72f
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-09-27 17:25:47 +0200

    Setup CI (#1)

--- examples/jest/test.example.ps1
+++ examples/jest/test.example.ps1
@@ -1,10 +1,12 @@
 # The path you cloned this repository to
 $REPO_PATH = "C:\Users\eps1lon\Development\nvda-snapshot-testing"
+# The path where NVDA speech output is logged. Need read/write access.
+$LOG_FILE_PATH = "$REPO_PATH\nvda.log"
 
 # End of configuration. Changing anything below at your own risk
 $NVDA_VENDOR = "$REPO_PATH\vendor\nvda-2020.2"
 $NVDA_BIN = "$NVDA_VENDOR\portable\nvda.exe"
-$Env:LOG_FILE_PATH = "$REPO_PATH\nvda.log"
+$Env:LOG_FILE_PATH = $LOG_FILE_PATH
 & $NVDA_BIN --log-file=$Env:LOG_FILE_PATH --config-path=$NVDA_VENDOR\settings
 yarn jest -c $REPO_PATH\examples\jest\jest.config.js
 & $NVDA_BIN -q
--- examples/node/test.example.ps1
+++ examples/node/test.example.ps1
@@ -1,9 +1,11 @@
 # The path you cloned this repository to
 $REPO_PATH = "C:\Users\eps1lon\Development\nvda-snapshot-testing"
+# The path where NVDA speech output is logged. Need read/write access.
+$LOG_FILE_PATH = "$REPO_PATH\nvda.log"
 
 # End of configuration. Changing anything below at your own risk
 $NVDA_VENDOR = "$REPO_PATH\vendor\nvda-2020.2"
 $NVDA_BIN = "$NVDA_VENDOR\portable\nvda.exe"
-& $NVDA_BIN --log-file=$REPO_PATH\nvda.log --config-path=$NVDA_VENDOR\settings
-node $REPO_PATH\examples\node\index.js $REPO_PATH\\nvda.log
+& $NVDA_BIN --log-file=$LOG_FILE_PATH --config-path=$NVDA_VENDOR\settings
+node $REPO_PATH\examples\node\index.js $LOG_FILE_PATH
 & $NVDA_BIN -q
