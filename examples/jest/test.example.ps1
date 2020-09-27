6eec7265ac900667b596adf692e6287aa06f10c8
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-09-27 09:56:49 +0200

    Vendor NVDA portable

--- examples/jest/test.example.ps1
+++ examples/jest/test.example.ps1
@@ -1,10 +1,10 @@
-# Where you installed NVDA
-$NVDA_BIN = "C:\Program Files (x86)\NVDA\nvda.exe"
 # The path you cloned this repository to
 $REPO_PATH = "C:\Users\eps1lon\Development\nvda-snapshot-testing"
 
 # End of configuration. Changing anything below at your own risk
+$NVDA_VENDOR = "$REPO_PATH\vendor\nvda-2020.2"
+$NVDA_BIN = "$NVDA_VENDOR\portable\nvda.exe"
 $Env:LOG_FILE_PATH = "$REPO_PATH\nvda.log"
-& $NVDA_BIN --log-file=$Env:LOG_FILE_PATH --config-path=$REPO_PATH\src\nvda-settings
+& $NVDA_BIN --log-file=$Env:LOG_FILE_PATH --config-path=$NVDA_VENDOR\settings
 yarn jest -c $REPO_PATH\examples\jest\jest.config.js
-nvda -q
+& $NVDA_BIN -q
--- examples/node/test.example.ps1
+++ examples/node/test.example.ps1
@@ -1,9 +1,9 @@
-# Where you installed NVDA
-$NVDA_BIN = "C:\Program Files (x86)\NVDA\nvda.exe"
 # The path you cloned this repository to
 $REPO_PATH = "C:\Users\eps1lon\Development\nvda-snapshot-testing"
 
 # End of configuration. Changing anything below at your own risk
-& $NVDA_BIN --log-file=$REPO_PATH\nvda.log --config-path=$REPO_PATH\src\nvda-settings
+$NVDA_VENDOR = "$REPO_PATH\vendor\nvda-2020.2"
+$NVDA_BIN = "$NVDA_VENDOR\portable\nvda.exe"
+& $NVDA_BIN --log-file=$REPO_PATH\nvda.log --config-path=$NVDA_VENDOR\settings
 node $REPO_PATH\examples\node\index.js $REPO_PATH\\nvda.log
-nvda -q
+& $NVDA_BIN -q
