c16f62eaafce94d108815eb7da70b278ff73681f
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-12-05 16:42:08 +0100

    Create separate artifacts for screen reader binaries

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,5 +1,14 @@
 # Contributing
 
+Before you install you need to make sure the install script uses the checked-in version of NVDA.
+You have to set the environment variable `SRTL_DEV` to `true` before you install local packages.
+Otherwise it will download the binaries from a the GitHub release for the currently checked out version of this library.
+
+```bash
+$ source .env.example
+$ yarn
+```
+
 ## Common workflows
 
 ### Bump NVDA
--- examples/node/test.example.ps1
+++ examples/node/test.example.ps1
@@ -2,10 +2,11 @@
 $REPO_PATH = "C:\Users\eps1lon\Development\nvda-snapshot-testing"
 # The path where NVDA speech output is logged. Need read/write access.
 $LOG_FILE_PATH = "$REPO_PATH\nvda.log"
+# The path where your package manager installed the "screen-reader-testing-library" package.
+$PACKAGE_PATH = "$REPO_PATH\node_modules\screen-reader-testing-library"
 
 # End of configuration. Changing anything below at your own risk
-$NVDA_VENDOR = "$REPO_PATH\src\nvda"
-$NVDA_BIN = "$NVDA_VENDOR\portable\nvda.exe"
-& $NVDA_BIN --log-file=$LOG_FILE_PATH --config-path=$NVDA_VENDOR\settings
+$NVDA_BIN = "$PACKAGE_PATH\bin\nvda.ps1"
+& $NVDA_BIN -logFile $LOG_FILE_PATH 
 node $REPO_PATH\examples\node\index.js $LOG_FILE_PATH
-& $NVDA_BIN -q
+& $NVDA_BIN -quit
